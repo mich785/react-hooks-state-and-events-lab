f3ffb9086213df3036072bbf611e408052cb6ab6
michelle nderi <nderimichelle@gmail.com>
2023-04-23 19:50:30 +0300

    Complete Assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React,{useState} from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false);
+  const [buttonText, setButtonText] = useState("Add to Cart");
+
+  const handleButtonClick = () => {
+    if (inCart) {
+      setInCart(false);
+      setButtonText("Add to Cart");
+    } else {
+      setInCart(true);
+      setButtonText("Remove From Cart");
+    }
+  };
+  
   return (
     <li className="">
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleButtonClick}>{buttonText}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,24 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
-  return (
-    <div className="ShoppingList">
-      <div className="Filter">
-        <select name="filter">
-          <option value="All">Filter by category</option>
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
-      </div>
-      <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
-      </ul>
-    </div>
-  );
+const [selectedCategory, setSelectedCategory] = useState("All");
+
+const handleChange = (event) => {
+setSelectedCategory(event.target.value);
+};
+
+const filteredItems = selectedCategory === "All"
+? items
+: items.filter(item => item.category === selectedCategory);
+
+return (
+<div className="ShoppingList">
+<div className="Filter">
+<select name="filter" value={selectedCategory} onChange={handleChange}>
+<option value="All">Filter by category</option>
+<option value="Produce">Produce</option>
+<option value="Dairy">Dairy</option>
+<option value="Dessert">Dessert</option>
+</select>
+</div>
+<ul className="Items">
+{filteredItems.map((item) => (
+<Item key={item.id} name={item.name} category={item.category} />
+))}
+</ul>
+</div>
+);
 }
 
 export default ShoppingList;
+
